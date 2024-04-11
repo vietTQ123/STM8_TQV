b564dfc569e3a65eda5f00e92c0bdba506959492
vietTQ123 <142866066+vietTQ123@users.noreply.github.com>
2024-04-11 22:06:47 +0700

    Add files via upload

---
+++ 5.STM8S_7SEGMENT_4DIGIT/main.c
@@ -0,0 +1,142 @@
+#include <stm8s.h>
+#include <stm8s_gpio.h>
+
+#define ledA GPIOC,GPIO_PIN_7
+#define ledB GPIOC,GPIO_PIN_6
+#define ledC GPIOC,GPIO_PIN_5
+#define ledD GPIOC,GPIO_PIN_4
+#define ledE GPIOC,GPIO_PIN_3
+#define ledF GPIOB,GPIO_PIN_4
+#define ledG GPIOB,GPIO_PIN_5
+
+#define digit4 GPIOD,GPIO_PIN_3
+#define digit3 GPIOD,GPIO_PIN_2
+#define digit2 GPIOA,GPIO_PIN_2
+#define digit1 GPIOA,GPIO_PIN_1
+
+void assert_failed(uint8_t* file, uint32_t line){}
+
+void delay(int t)
+{
+  int i, j;
+  for (i = 0; i < t; i++)
+  {
+    for (j = 0; j < 400; j++){}
+  }
+}
+
+void GPIO_CONFIGURATION()
+{
+ GPIO_DeInit(GPIOD);
+ GPIO_DeInit(GPIOB);
+ GPIO_DeInit(GPIOC);
+ GPIO_DeInit(GPIOA);
+ 
+ GPIO_Init(ledA,GPIO_MODE_OUT_PP_LOW_FAST);
+ GPIO_Init(ledB,GPIO_MODE_OUT_PP_LOW_FAST);
+ GPIO_Init(ledC,GPIO_MODE_OUT_PP_LOW_FAST);
+ GPIO_Init(ledD,GPIO_MODE_OUT_PP_LOW_FAST);
+ GPIO_Init(ledE,GPIO_MODE_OUT_PP_LOW_FAST);
+ GPIO_Init(ledF,GPIO_MODE_OUT_PP_LOW_FAST);
+ GPIO_Init(ledG,GPIO_MODE_OUT_PP_LOW_FAST);
+ 
+ GPIO_Init(digit1,GPIO_MODE_OUT_PP_LOW_FAST);
+ GPIO_Init(digit2,GPIO_MODE_OUT_PP_LOW_FAST);
+ GPIO_Init(digit3,GPIO_MODE_OUT_PP_LOW_FAST);
+ GPIO_Init(digit4,GPIO_MODE_OUT_PP_LOW_FAST);
+}
+
+unsigned char led7Hex[]={0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90};
+void LED7_writeByte (unsigned char b)
+{
+  if(((b>>0)& 0x01)==0)
+  {
+    GPIO_WriteLow(ledA);
+  }
+  else
+  {
+    GPIO_WriteHigh(ledA);
+  }
+  if(((b>>1)& 0x01)==0)
+  {
+    GPIO_WriteLow(ledB);
+  }
+  else
+  {
+    GPIO_WriteHigh(ledB);
+  }
+  if(((b>>2)& 0x01)==0)
+  {
+    GPIO_WriteLow(ledC);
+  }
+  else
+  {
+    GPIO_WriteHigh(ledC);
+  }
+  if(((b>>3)& 0x01)==0)
+  {
+    GPIO_WriteLow(ledD);
+  }
+  else
+  {
+    GPIO_WriteHigh(ledD);
+  }
+  if(((b>>4)& 0x01)==0)
+  {
+    GPIO_WriteLow(ledE);
+  }
+  else
+  {
+    GPIO_WriteHigh(ledE);
+  }
+  if(((b>>5)& 0x01)==0)
+  {
+    GPIO_WriteLow(ledF);
+  }
+  else
+  {
+    GPIO_WriteHigh(ledF);
+  }
+  if(((b>>6)& 0x01)==0)
+  {
+    GPIO_WriteLow(ledG);
+  }
+  else
+  {
+    GPIO_WriteHigh(ledG);
+  }
+}
+
+int main( void )
+{
+ int i,j;
+ GPIO_CONFIGURATION();
+ while(1)
+ {
+  for(i = 0; i<=9999; i++)
+  {
+   for(j=0;j<=50;j++)
+   {
+     GPIO_WriteHigh(digit1);
+     LED7_writeByte(led7Hex[i%10]);
+     delay(10);
+     GPIO_WriteLow(digit1);
+     
+     GPIO_WriteHigh(digit2);
+     LED7_writeByte(led7Hex[i/10%10]);
+     delay(10);
+     GPIO_WriteLow(digit2);
+     
+     GPIO_WriteHigh(digit3);
+     LED7_writeByte(led7Hex[i/100%10]);
+     delay(10);
+     GPIO_WriteLow(digit3);
+     
+     GPIO_WriteHigh(digit4);
+     LED7_writeByte(led7Hex[i/1000]);
+     delay(10);
+     GPIO_WriteLow(digit4);
+   }
+  }
+ } 
+}
